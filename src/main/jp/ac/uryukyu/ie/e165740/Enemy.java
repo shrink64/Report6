368963afe0658e4e8b52b5379e37bce21d0c7318
Tatsuya Iwanaga <e165740@ie.u-ryukyu.ac.jp>
2016-11-25 16:06:08 +0900

    hoge

--- src/main/jp/ac/uryukyu/ie/e165740/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e165740/Enemy.java
@@ -46,9 +46,12 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
+        if(hitPoint <= 0)
+            return;
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+        if(hitPoint > 0)
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
     }
 
     /**
