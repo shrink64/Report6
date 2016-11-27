117dfa1c05556d12398b2e953269eeb0a92de382
Tatsuya Iwanaga <e165740@ie.u-ryukyu.ac.jp>
2016-11-27 19:06:29 +0900

    hoge

--- src/test/jp/ac/uryukyu/ie/e165740/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e165740/EnemyTest.java
@@ -5,18 +5,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EnemyTest {
-    /**
-     * 「死んでる状態では攻撃できない」ことを確認。
-     * @throws Exception
-     */
+
     @Test
-    public void getAttack() throws Exception {
+    public void attack() throws Exception {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
         enemy.setIsDead(true);
         for(int i=0; i<10; i++) {
-            enemy.setAttack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
         assertEquals(heroHP, hero.getHitPoint());
     }
