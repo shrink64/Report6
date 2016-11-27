1f32b5d143fc76052f901e2311da6fd7c71684ae
Tatsuya Iwanaga <e165740@ie.u-ryukyu.ac.jp>
2016-11-27 16:19:02 +0900

    hoge

--- src/test/jp/ac/uryukyu/ie/e165740/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e165740/EnemyTest.java
@@ -10,13 +10,13 @@ public class EnemyTest {
      * @throws Exception
      */
     @Test
-    public void attack() throws Exception {
+    public void getAttack() throws Exception {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
-        Enemy enemy = new Enemy("テストスライム",6 , 3);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
         enemy.setIsDead(true);
         for(int i=0; i<10; i++) {
-            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+            enemy.setAttack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
         assertEquals(heroHP, hero.getHitPoint());
     }
